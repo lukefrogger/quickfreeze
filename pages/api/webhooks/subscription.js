b7b4c0f301387b1d6e22c68545c317c6c54f66de
lukefrogger <luke.frauhiger@gmail.com>
2021-09-23 22:19:43 -0500

    for dates?

--- pages/api/webhooks/subscription.js
+++ pages/api/webhooks/subscription.js
@@ -89,8 +89,8 @@ async function createSubscription(stripeSub) {
 			status: "active",
 			product: products[0].id,
 			cancel_at_period_end: stripeSub.cancel_at_period_end,
-			current_period_start: new Date(stripeSub.current_period_end),
-			current_period_end: new Date(stripeSub.current_period_start),
+			current_period_start: stripeSub.current_period_end,
+			current_period_end: stripeSub.current_period_start,
 		};
 		const { error: subError } = await supabaseAdmin.from("subscriptions").insert(newSub, { returning: "minimal" });
 		if (subError) {
