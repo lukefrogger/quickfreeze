676ddf618645a6edc4eb61dd8c7a3b0f9f8e9f30
lukefrogger <luke.frauhiger@gmail.com>
2021-09-30 17:41:56 -0500

    log token

--- pages/api/ice_cubes/[endpoint].js
+++ pages/api/ice_cubes/[endpoint].js
@@ -47,6 +47,7 @@ async function verifyApiToken(token) {
 	if (!token || !token.includes("QF_")) {
 		false;
 	}
+	console.log(token);
 	const formatted = token.split("Bearer ")[1];
 	const secret = formatted.split("QF_")[1];
 
